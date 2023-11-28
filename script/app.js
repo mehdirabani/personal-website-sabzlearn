f532e2330fd0d0c4ee82f6b513a9589bb4746156
mehdi rabani <pgm.mehdirabani@gmail.com>
2023-11-28 12:31:18 +0330

    fix nav  toggle

--- script/app.js
+++ script/app.js
@@ -1,5 +1,5 @@
 const navToggle = document.querySelector(".nav__toggle");
 
 navToggle.addEventListener("click", function () {
-  navToggle.clssList.toggle("nav__toggle--open");
+  navToggle.classList.toggle("nav__toggle--open");
 });
