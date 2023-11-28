2514d56824f6a5be098d1b6cecc87708a0e633d5
mehdi rabani <pgm.mehdirabani@gmail.com>
2023-11-28 12:48:29 +0330

    Feat Menu Mobile

--- script/app.js
+++ script/app.js
@@ -1,5 +1,7 @@
 const navToggle = document.querySelector(".nav__toggle");
+const menuMobile = document.querySelector(".menu");
 
 navToggle.addEventListener("click", function () {
   navToggle.classList.toggle("nav__toggle--open");
+  menuMobile.classList.toggle("menu--open");
 });
