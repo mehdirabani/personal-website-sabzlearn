621ba91d16882c51446e42a62f3c979a667628a4
Mydi Rabani <pgm.mehdirabani@gmail.com>
2023-12-11 23:10:07 +0330

    Add Js Resume List _items

--- script/app.js
+++ script/app.js
@@ -1,7 +1,17 @@
 const navToggle = document.querySelector(".nav__toggle");
 const menuMobile = document.querySelector(".menu");
+const resumeListItems = document.querySelectorAll(".resume-list__item");
 
 navToggle.addEventListener("click", function () {
   navToggle.classList.toggle("nav__toggle--open");
   menuMobile.classList.toggle("menu--open");
+});
+
+resumeListItems.forEach((resumeListItem) => {
+  resumeListItem.addEventListener("click", function () {
+    document
+      .querySelector(".resume-list__item--active")
+      .classList.remove("resume-list__item--active");
+    this.classList.add("resume-list__item--active");
+  });
 });
