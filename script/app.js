6a98b7e5cacaf5bda7f4499887acd896012dbd8b
Mydi Rabani <pgm.mehdirabani@gmail.com>
2023-12-14 21:44:56 +0330

    Style ul Portflio

--- script/app.js
+++ script/app.js
@@ -1,18 +1,28 @@
 const navToggle = document.querySelector(".nav__toggle");
 const menuMobile = document.querySelector(".menu");
 const resumeListItems = document.querySelectorAll(".resume-list__item");
-
 navToggle.addEventListener("click", function () {
   navToggle.classList.toggle("nav__toggle--open");
   menuMobile.classList.toggle("menu--open");
 });
-
 resumeListItems.forEach((resumeListItem) => {
   resumeListItem.addEventListener("click", function () {
-    document.querySelector(".resume-list__item--active").classList.remove("resume-list__item--active");
-    document.querySelector(".resume__content--show").classList.remove("resume__content--show");
+    document
+      .querySelector(".resume-list__item--active")
+      .classList.remove("resume-list__item--active");
+    document
+      .querySelector(".resume__content--show")
+      .classList.remove("resume__content--show");
     this.classList.add("resume-list__item--active");
     let contentid = this.getAttribute("deta-content-id");
     document.querySelector(contentid).classList.add("resume__content--show");
   });
+});
+// slider
+const swiper = new swiper(".swiper", {
+  direction: "vertical",
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
